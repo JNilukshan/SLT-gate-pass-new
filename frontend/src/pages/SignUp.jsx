7a56271612d9dbcf6ef47ab8149d0a42fdaa8444
Sandeepa Gamaarachchi <139548679+SandeepaGamaarachchi@users.noreply.github.com>
2025-06-16 11:31:52 +0530

    Update SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { motion } from "framer-motion";
-import slt from "../assets/SLTMobitel_logo.svg";
+import slt from "../assets/SLTMobitel_Logo.svg";
 import sltHome from "../assets/sltHome.webp"; // Import background image
 
 const Signup = () => {
