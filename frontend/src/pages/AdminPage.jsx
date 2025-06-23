d7ace945abc81422f798878a10877469c225e3ac
S H M J N Sooriyahetti <142863578+JNilukshan@users.noreply.github.com>
2025-06-23 14:59:17 +0530

    Update AdminPage.jsx

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -21,7 +21,7 @@ const AdminPage = () => {
     formData.append('csvFile', csvFile);
 
     try {
-      const response = await axios.post('http://localhost:5000/api/upload/upload', formData, {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/upload/upload`, formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
 
