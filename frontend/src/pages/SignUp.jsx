3ebd6d0f46f1904b27b08ecebef8ac3004b97e9e
S H M J N Sooriyahetti <142863578+JNilukshan@users.noreply.github.com>
2025-06-23 15:17:23 +0530

    Update SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -17,7 +17,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/auth/signup", formData);
+      await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/signup`, formData);
       alert("Account created successfully!");
       navigate("/login");
     } catch (error) {
